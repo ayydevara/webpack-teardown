6986645fb46f13da00f0d9bef91ed221b48632ba
Sreekanth Ayydevara <sayydevara@gmail.com>
2017-01-03 18:47:26 -0800

    initial checkin

---
+++ src/app/app.routing.ts
@@ -0,0 +1,5 @@
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+export const routes: Routes = [];
+
+export const routing = RouterModule.forRoot(routes);
